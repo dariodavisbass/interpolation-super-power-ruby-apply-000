9faff4574fe44411fe63dcb06d44c6805fa9e8e7
dauntless-device-4791 <dariodavisbass@gmail.com>
2020-08-24 20:49:50 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,1 @@
 # Write your #display_rainbow method here
-puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
-def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
-end
-puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
-end
